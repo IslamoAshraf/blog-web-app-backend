9ac971b0ea28f0a6321371bc1681194503f1a77e
IslamAshraf <islameliwa@gmail.com>
2021-09-28 00:32:24 +0200

    user model and user schema

---
+++ modules/users/model/user.model.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+const userSchema = require("../schema/user.schema");
+
+const User = mongoose.model("User", userSchema);
---
+++ modules/users/schema/user.schema.js
@@ -0,0 +1,11 @@
+const { Schema } = require("mongoose");
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  location: { type: String, required: true },
+  role: { type: String, default: "user" },
+});
+
+module.exports = userSchema;
