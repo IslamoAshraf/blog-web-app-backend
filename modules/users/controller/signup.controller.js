a730141ad082dad31868c14669cf5f9f9f92eec9
IslamAshraf <islameliwa@gmail.com>
2021-11-15 00:04:01 +0200

    refactor sign up email verifiction with try and catch syntax

--- modules/users/controller/signup.controller.js
+++ modules/users/controller/signup.controller.js
@@ -7,34 +7,33 @@ module.exports = async (req, res) => {
   try {
     const { username, email, password, phone, location } = req.body;
     //check if user exist
-    const isExist = await User.findOne({ email });
-    if (isExist) {
-      return res
-        .status(StatusCodes.BAD_REQUEST)
-        .json({ error: "user is already exist" });
-    }
+    // const isExist = await User.findOne({ email });
+    // if (isExist) {
+    //   return res
+    //     .status(StatusCodes.BAD_REQUEST)
+    //     .json({ error: "user is already exist" });
+    // }
     //hash password
     bcrypt.hash(password, 8, async (err, hash) => {
       if (err) {
         return res.json({ err });
       }
       //check if email server is ready to sent confirmation message
-      await mailer(email, username)
-        .then(async () => {
-          const user = await User.insertMany({
-            username,
-            email,
-            password: hash,
-            phone,
-            location,
-          });
-          return res
-            .status(StatusCodes.CREATED)
-            .json({ message: "register successfully", user });
-        })
-        .catch((error) => {
-          return res.status(StatusCodes.BAD_REQUEST).json({ error });
+      try {
+        await mailer(email, username);
+        const user = await User.insertMany({
+          username,
+          email,
+          password: hash,
+          phone,
+          location,
         });
+        return res
+          .status(StatusCodes.CREATED)
+          .json({ message: "register successfully", user });
+      } catch (error) {
+        return res.status(StatusCodes.BAD_REQUEST).json({ error });
+      }
     });
   } catch (error) {
     return res.status(StatusCodes.BAD_REQUEST).json({ error });
